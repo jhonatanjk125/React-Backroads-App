4a62152037fbd872cf54d3aab5cdb074c1987ce2
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-10-28 23:13:05 -0500

    Add tours component

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpeg';
+import tour2 from './images/tour-2.jpeg';
+import tour3 from './images/tour-3.jpeg';
+import tour4 from './images/tour-4.jpeg';
+
 export const pageLinks = [
     {id:1, href:'#home', text: 'home'},
     {id:2, href:'#about', text: 'about'},
@@ -30,4 +35,47 @@ export const services = [
     title:'amazing comfort', 
     text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'
 },
+]
+
+export const tours = [
+    {
+        id:1,
+        image: tour1,
+        date:'august 26th, 2024',
+        title:'Tibet Adventure',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location:'China',
+        duration:6,
+        cost:2100,
+    },
+    {
+        id:2,
+        image: tour2,
+        date:'october 1st, 2024',
+        title:'Best Of Java',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location:'Indonesia',
+        duration:11,
+        cost:1400,
+    },
+    {
+        id:3,
+        image: tour3,
+        date:'September 15th, 2024',
+        title:'Explore Hong Kong',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location:'Hong Kong',
+        duration:8,
+        cost:5000,
+    },
+    {
+        id:4,
+        image: tour4,
+        date:'December 5th, 2023',
+        title:'Kenya Highlights',
+        info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location:'Kenya',
+        duration:20,
+        cost:4500,
+    },
 ]

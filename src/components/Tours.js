0f0c268cab5c6ba202dab4c1e2ff086b1571f801
jhonatanjk125 <jhonatanjk125@gmail.com>
2023-10-28 21:39:57 -0500

    Set up title components

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,10 +1,9 @@
+import Title from "./Title"
+
 const Tours = () => {
   return (
     <section className="section" id="tours">
-    <div className="section-title">
-      <h2>featured <span>tours</span></h2>
-    </div>
-
+        <Title title="about" subTitle="us" />
     <div className="section-center featured-center">
       <article className="tour-card">
         <div className="tour-img-container">
